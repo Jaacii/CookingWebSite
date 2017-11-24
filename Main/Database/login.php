e49f9a577e7b067590e83d2592864169c88343c9
Jaacii <sliphno@hotmail.de>
2017-11-24 16:20:10 +0100

    sad try to make the login work

---
+++ Main/Database/login.php
@@ -0,0 +1,16 @@
+
+<?php
+
+
+// Create connection
+$con=mysqli_connect(host,username,password,dbname); 
+
+
+
+// Check connection
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+?> 
+
