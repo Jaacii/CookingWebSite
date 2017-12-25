74cad1b8361cce38b1df6644413f74f8b874cbcc
Jaacii <sliphno@hotmail.de>
2017-12-25 19:09:18 +0100

    random works perfect :) only the recipe side need to be edited

--- Main/Database/randomizer.php
+++ Main/Database/randomizer.php
@@ -28,11 +28,15 @@ echo "</br>" . $row["pic"]. "  " . $row["foodname"]. "   difficulty:   " . $row[
 $range_result = mysqli_query( $db ," SELECT MAX(`id`) AS max_id , MIN(`id`) AS min_id FROM `recipe2` ");
 $range_row = mysqli_fetch_object( $range_result );
 $random = mt_rand( $range_row->min_id , $range_row->max_id );
-$result = mysqli_query( $db, " SELECT * FROM `recipe2` WHERE `id` >= $random LIMIT 5 "); 
+$sql = mysqli_query( $db, " SELECT * FROM `recipe2` WHERE `id` >= $random LIMIT 5 "); 
 
-while ($result = $result->fetch_assoc()) {
-    echo "<br>" . $result['foodname']. "  | difficulty:  ".$result['difficulty'] . "  |  rating: ". $result['rating'] . "<br>";
-}
+//$result = $db->query($sql);
+$row = mysqli_fetch_assoc($sql);
+
+
+    echo "<br>" . $row['foodname']. "  | difficulty:  ".$row['difficulty'] . "  |  rating: ". $row['rating'] . "<br>";
+	  echo "" . $row['image']. "  <br><br>ingredients: ".$row['ingredients'] . "    <br><br>preparation: ". $row['preparation'] . "<br>";
+	
 
 
 
