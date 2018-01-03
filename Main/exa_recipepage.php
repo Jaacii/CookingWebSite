31e3180e4602078d29bcf7427b8b96024db7ce44
Jaacii <sliphno@hotmail.de>
2018-01-03 13:34:25 +0100

    exa works with search

--- Main/exa_recipepage.php
+++ Main/exa_recipepage.php
@@ -5,59 +5,51 @@
 <head >
 	<meta charset= "utf-8">
 	<title>Kochdatenbank</title>
-	<link rel="stylesheet" href=stylesheet.css>
+	<link rel="stylesheet" href= stylesheet.css>
 </head>
-
 <body>
+<header class=headclass> <!--Logo-->
+ <h1>Kochdatenbank</h1>
 <nav> 
  <div class=navigation>
   <a href ="mainview.php">| Home |</a> <a href ="search.php"> Search |</a>  <a href ="random.php"> Random |</a> <a href ="contactview.php"> Contact |</a> <a href ="aboutview.php"> About |</a> <a href ="test.php"> Members |</a><a href ="allrecipeview.php"> Recipes |</a><!-- -->
  </nav>
+ 
+ </header>
+<!--  Search form -->
 <div class=sidebar>
 <?php
 
-/*
-//wie kriege ich die idee beim anklicken automatisch aktualisiert.
-$id =1;													//get id von suche oder vom drauf klicken auf bild??
-$db=mysql_connect  ('localhost', 'root', '', 'cooking4you');
-if ($db->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-$sql = "SELECT   ingredients ,difficulty, rating  FROM testrecipe WHERE id = $id";
-$result = $db->query($sql);
-if ($result->num_rows > 0) {
-   // Jede Reihe ausgeben
-   while($row = $result->fetch_assoc()) {
-	   echo "</br>Ingredients: " . $row["ingredients"].  "  </br> difficulty:   " . $row["difficulty"]. " </br>rating:   " . $row["rating"]. "<br>";
-		}
-} else {
-    echo "No recipes uploaded yet.";
-}
-*/
+
+
+$db = mysqli_connect('localhost', 'root', '', 'cooking4you');
+
 ?>
 
 </div>
 <div class=maincontent>
-<img src="img_placeholder/KimchiPlaceholder.jpg" alt="Kimchi" width="350" height="230">
+<!--img src="img_placeholder/KimchiPlaceholder.jpg" alt="Kimchi" width="350" height="230"-->
 <?php
 $food_id = $_GET['food_id'];
-echo $food_id;
+
+
+$sql = mysqli_query( $db, " SELECT * FROM `recipe2` WHERE `id` = '$food_id'"); 
+$row = mysqli_fetch_assoc($sql);
 
 /*
-$sql2 = "SELECT   pic ,foodname, preparation  FROM testrecipe WHERE id = $id";
-$result2 = $db->query($sql2);
-if ($result2->num_rows > 0) {
-   // Jede Reihe ausgeben
-   while($row = $result->fetch_assoc()) {
-	   echo "</br>" . $row["pic"].  "  </br> Recipe for:   " . $row["foodname"]. " </br></br>preparation:  </br> " . $row["preparation"]. "<br>";
-		}
-} else {
-    echo "No recipes uploaded yet.";
-}
 
-$conn->close();
-
+$sql = "SELECT  * FROM recipe2 WHERE id = $food_id";
+$result = $db->query($sql);
+//$row = mysqli_fetch_assoc($sql);
 */
+	  echo "<br>" . $row['foodname']. "  | difficulty:  ".$row['difficulty'] . "  |  rating: ". $row['rating'] . "<br>";
+	    echo "" . '<img src="'.$row['image'].'">'. "  <br><br>ingredients: ".$row['ingredients'] . "    <br><br>preparation: ". $row['preparation'] . "<br>";
+ 
+
+
+$db->close();
+
+
 ?>
 </div>
 
